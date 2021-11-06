b8905abe3e243668199c221dc631b1ee0ab011fb
RamirodePiscis <89928000+RamirodePiscis@users.noreply.github.com>
2021-11-06 00:30:13 -0400

    Add files via upload

---
+++ src/components/MateriaCarga/Componentes/Editar/Editar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Edit } from './EditarFormulario/Edit'
+
+export const Editar = () => {
+    return (
+        <div>
+            <Edit />
+        </div>
+    )
+}
