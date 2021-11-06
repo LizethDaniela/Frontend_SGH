a5875ee9a53cb7b935ca944138b711959bb4f564
celiaoyolaramos <90815880+celiaoyolaramos@users.noreply.github.com>
2021-11-06 00:20:43 -0400

    direcciones

--- src/components/RouterApp/RouterApp.js
+++ src/components/RouterApp/RouterApp.js
@@ -9,6 +9,8 @@ import { LoginComponent } from '../auth/LoginComponent';
 import { RegisterEstudentsComponent } from '../auth/RegisterEstudentsComponent';
 import { RegisterDocentsComponent } from '../auth/RegisterDocentsComponent';
 import { AsignarMateriasComponent } from '../AsignarMaterias/AsignarMateriasComponent';
+import { HorariosComponent } from '../Horarios/HorariosComponent';
+import { ModificarHorarioComponent } from '../Horarios/ModificarHorarioComponent';
 import { DashBoardComponent } from '../dashboard/DashBoardComponent';
 import { MainDashBoard } from '../dashboard/MainDashBoard';
 import { Autenticacion } from '../auth/Autenticacion';
@@ -24,6 +26,8 @@ export const RouterApp = () => {
                 <Route exact path="/registerdocentes" component= {RegisterDocentsComponent}/>
                 <Route exact path="/registerestudents" component={RegisterEstudentsComponent}/>
                 <Route exact path="/asignarmaterias" component={AsignarMateriasComponent}/>
+                <Route exact path="/horarios" component={HorariosComponent}/>
+                <Route exact path="/modificar" component={ModificarHorarioComponent}/>
                 <Route exact path="/Dashboard" component={DashBoardComponent}/>
                 <Route path="/main" component={MainDashBoard}/>
                 <Route path="/" component={Autenticacion}/>
