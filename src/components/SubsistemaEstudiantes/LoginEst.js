53cf3c1a9c84f6c99412951c09c88a229aa6a0f3
LizethDaniela <quispelopez007@gmail.com>
2021-12-13 11:59:31 -0400

    ajuste de horario

--- src/components/SubsistemaEstudiantes/LoginEst.js
+++ src/components/SubsistemaEstudiantes/LoginEst.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+/*import React, { useEffect } from 'react'
 import { MdPersonPin, MdVpnKey } from 'react-icons/md';
 import { useForm } from '../../hooks/useForm';
 import { useSelector } from 'react-redux';
@@ -10,13 +10,13 @@ import { useDispatch } from 'react-redux';
 export const LoginEst = () => {
     const history = useNavigate();
     const dispatch = useDispatch();
-   /* useEffect(() => {
+    /* useEffect(() => {
         const userData = localStorage.getItem("user");
         if (userData != null) {
             dispatch(auth(JSON.parse(userData)));
         }
     }, []);*/
-    const { auth: authRename} = useSelector(state => state);
+    /*const { auth: authRename} = useSelector(state => state);
     const { user, msnerror } = authRename;
     console.log(user);
     const [form, handlerChangeForm, handlerResetForm] = useForm({
@@ -81,10 +81,100 @@ export const LoginEst = () => {
                             </div>
                         </form>
                     </div>
-                   
                 </div>
             </div>
         </div>) : (<>{ history("/estudent/main")}</>)}
+    </>
+    );
+};*/
+
+
+import React, { useEffect } from 'react'
+import { MdPersonPin, MdVpnKey } from 'react-icons/md';
+import { useForm } from '../../hooks/useForm';
+import { useSelector } from 'react-redux';
+import { authAsync, auth } from '../../actions/auth';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+
+export const LoginEst = () => {
+    //const history = useNavigate();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const userData = localStorage.getItem("user");
+        if (userData != null) {
+            dispatch(auth(JSON.parse(userData)));
+        }
+    }, []);
+    const { auth: authRename} = useSelector(state => state);
+    const { user, msnerror } = authRename;
+    console.log(user);
+    const [form, handlerChangeForm, handlerResetForm] = useForm({
+        username: "Pepito",
+        password: "1234"
+    });
+    const {username, password} = form;
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        dispatch(authAsync(username, password));
+    };
+    return (
+    <>
+        
+        
+        <div className="container">
+            <div className="d-flex justify-content-center h-100">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Login Estudiante</h3>
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={handlerSubmit}>
+                            <div className="input-group form-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <MdPersonPin />
+                                    </span>
+                                </div>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="username" 
+                                    autoComplete="off"
+                                    name="username" 
+                                    value={username} 
+                                    onChange={handlerChangeForm}
+                                />
+                            </div>
+                            <div className="input-group form-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <MdVpnKey />
+                                    </span>
+                                </div>
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    placeholder="password" 
+                                    autoComplete="off"
+                                    name="password" 
+                                    value={password} 
+                                    onChange={handlerChangeForm}
+                                />
+                            </div>
+                            <div>{msnerror}</div>
+                            
+                            <div className="form-group">
+                                <div className="col-md-12 text-center">
+                                    <input type="submit" value="Login" className="btn float-rigth login_btn" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
     );
 };
--- src/components/SubsistemaEstudiantes/VerHorariosEst.js
+++ src/components/SubsistemaEstudiantes/VerHorariosEst.js
@@ -2,21 +2,70 @@ import React from 'react'
 
 import Head from '../head/Head';
 import { DashEstudiante } from './DashEstudiante';
+import './stylos.css';
 
 
 
 
 export const VerHorariosEst= () => {
-   
     return (
-       <>
-          
-            <Head/>
-            <DashEstudiante/>
-            <div className="container">
-                aqui el contenido para ver horarios ahora si 
-            </div>
-           
-       </>
+        <>
+        <Head/>
+        <DashEstudiante/>
+        <div className="container">
+            <table className="tabla">
+                <thead className="head">
+                    <td>HORA</td>
+                    <td>LUNES</td>
+                    <td>MARTES</td>
+                    <td>MIERCOLES</td>
+                    <td>JUEVES</td>
+                    <td>VIERNES</td>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>7:45-10:00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>Estadistica 1</td>
+                </tr>
+                <tr>
+                    <td>10:00-12:15</td>
+                    <td></td>
+                    <td>Calculo 1</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>12:15-14:00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>14:00-16:15</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>16:15-18:30</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </>
     );
 };
